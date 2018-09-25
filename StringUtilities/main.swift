4c795a538ac261a88bbdfe7fd989be58b7587613
Joe Farrell <disastrousgamemaster@gmail.com>
2018-09-25 16:06:23 -0400

    Initial Commit

---
+++ StringUtilities/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StringUtilities
+//
+//  Created by Joe Farrell on 9/25/18.
+//  Copyright © 2018 Joe Farrell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
