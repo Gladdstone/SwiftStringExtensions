796294f159fbea3a8b1c1cd1f745271fce8ea8ec
Joe Farrell <disastrousgamemaster@gmail.com>
2018-09-25 16:10:51 -0400

    String extensions to Swift

--- StringUtilities/main.swift
+++ StringUtilities/main.swift
@@ -10,3 +10,42 @@ import Foundation
 
 print("Hello, World!")
 
+extension String {
+    func index(of: String) -> Int {
+        var index = 0
+        
+        for char in self {
+            if of != "" && Character(String(of.prefix(1))) == char {
+                let ln = of.count + index
+                
+                if self[index..<ln] == of {
+                    return index
+                }
+            }
+            
+            index += 1
+        }
+        
+        return -1
+    }
+    
+    subscript(bounds: CountableClosedRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        
+        return String(self[start...end])
+    }
+    
+    subscript(bounds: CountableRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end: String.Index
+        
+        if bounds.upperBound > distance(from: startIndex, to:endIndex) {
+            end = endIndex
+        } else {
+            end = index(startIndex, offsetBy: bounds.upperBound)
+        }
+        
+        return String(self[start..<end])
+    }
+}
